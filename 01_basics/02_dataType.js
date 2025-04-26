942cfd6c70703895304fd933ee2c74bc7e913bed
Rohan Roy <122220768+Rohanroy5104@users.noreply.github.com>
2025-04-26 19:24:42 +0000

    datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; // traet all js code as newer version
+
+// alert(3 + 3) // we are using nodejs not browser
+
+console.log(3
+    +
+    3
+)  // code readability should be high
+
+console.log("Hitesh")
+
+let name = "Hitesh"
+let age = 18
+let isLoginIn = false
+
+/*
+Different types of datatypes:
+number 2 to the power 53
+bigint: used when numbers are too big
+string = ""
+boolean: true/false
+null: standalone value
+undefined: value is not assigned
+*/
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

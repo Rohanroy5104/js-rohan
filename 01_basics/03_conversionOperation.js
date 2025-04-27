2a662977b65ca8dd7bd54a11b4ac910133129b43
Rohan Roy <122220768+Rohanroy5104@users.noreply.github.com>
2025-04-27 06:34:19 +0000

    More Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 /*
 Notes:
@@ -26,5 +26,25 @@ let booleanIsLoggedIn = Boolean(isLogedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *******************************************Operations***************************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Hello"
+let str2 = "Rohan"
+let str3 = str1 + " " +str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
